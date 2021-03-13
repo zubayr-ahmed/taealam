e53fb27730b5f77bbff746bbd55989f8c53ef499
AHMED ZBYR <ahmedzbyr@gmail.com>
2021-03-13 19:07:11 +0000

    update

--- datasci_learning/tweeter_senti_analysis.py
+++ datasci_learning/tweeter_senti_analysis.py
@@ -1,21 +1,20 @@
 import tweepy
 from textblob import TextBlob
 
-consumer_key = "AfYZuGCup1sBXO7Xv00x4yUFC"
-consumer_secret = "1KEa6Z63HDlqQuXMIkBCiM3NR5cksOZ9GoGzRQwRAOGhk1TMny"
+consumer_key = "AfYZxxxxxxxxxxxxxxxxxx4yUFC"
+consumer_secret = "1KEa6Zxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxhk1TMny"
 
-access_token = "144773310-7EfM5mqrLLr3T0j05x5gieE1JkY1997NwxJMHyiS"
-access_token_secret = "BuoBjnsXH8esAtwZWMznKDk3C4NsTYhWLlugREKkExJin"
+access_token = "144xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxHyiS"
+access_token_secret = "BuoBxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxkExJin"
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
-api= tweepy.API(auth)
+api = tweepy.API(auth)
 
 public_tweets = api.search('india')
 
 for tweet in public_tweets:
-    print tweet.text
+    print(tweet.text)
     analysis = TextBlob(tweet.text)
-    print analysis.sentiment
-
+    print(analysis.sentiment)
